12b33f5f04a35c70c3ccb5826794552349833df2
Steve Purves <steve@curvenote.com>
2026-04-24 15:45:24 +0100

    🚀 v0.1.3 from b1328c8

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-const version = "0.1.2";
+const version = "0.1.3";
 const __dirname$1 = path.dirname(fileURLToPath(import.meta.url));
 const isProduction = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 3050;
