a29fdbca60b648c35edc915c35ac23226250db29
Steve Purves <steve@curvenote.com>
2026-08-20 08:31:56 +0100

    🚀 v0.2.3 from 707dab8

---
+++ client/assets/swimlanesDiagram-ULZ7WXOC-D3YFlASq.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-BpeU6T36.js";import{_ as a}from"./mermaid.core-C4dykGjT.js";import"./chunk-5VM5RSS4-Cp03yAQ8.js";import"./chunk-XXDRQBXY-YlDAr4Tw.js";import"./chunk-KBJHAD2P-C5RKQJ1b.js";import"./chunk-2GRJ4B5K-DWa5duSe.js";import"./channel-D4TrqAfj.js";import"./index-fWuzAo0C.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
